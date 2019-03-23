8153146c68975388c52c63426a5f3f65e540accd
ajha <anujjha1@in.ibm.com>
2019-03-23 23:29:32 +0530

    Implement quickfind

--- src/com/kotlin/learn/Test.kt
+++ src/com/kotlin/learn/Test.kt
@@ -1,9 +1,15 @@
 package com.kotlin.learn
 
 import com.kotlin.learn.arrays.Lambdas
+import com.kotlin.learn.princetoncourse.QuickFind
 
 fun main(word: Array<String>){
-    println("Hello ");
-    var prob = Lambdas("Anuj");
-    println(prob.getNum(4));
+    println("Hello ")
+    var prob = Lambdas("Anuj")
+    println(prob.getNum(4))
+    var quick = QuickFind(5)
+    println(quick.isConnected(2,3))
+    quick.union(2,3)
+    println(quick.isConnected(2,3))
+
 }
